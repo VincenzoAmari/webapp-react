f27d946def9e19fde657d7576d4f2970c6ad4c7e
VincenzoAmari <vincenzo.am99@gmail.com>
2025-03-17 17:50:19 +0100

    simulazione chiamata

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,21 @@
 export default function HomePage() {
+  const movies = [
+    { id: 1, title: "The Matrix", director: "Wachowski", release_year: 1999 },
+    { id: 2, title: "Inception", director: "Nolan", release_year: 2010 },
+  ];
+
   return (
     <div>
       <h1>Home Page - Lista dei Film</h1>
+      <ul className="list-group">
+        {movies.map((movie) => (
+          <li key={movie.id} className="list-group-item">
+            <h2>{movie.title}</h2>
+            <p>Direttore: {movie.director}</p>
+            <p>Anno: {movie.release_year}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }

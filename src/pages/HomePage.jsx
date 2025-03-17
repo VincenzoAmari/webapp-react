1551658630819b4e4d258582a301ac92fd0606ca
VincenzoAmari <vincenzo.am99@gmail.com>
2025-03-17 17:43:40 +0100

    home page e movie page

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Home Page - Lista dei Film</h1>
+    </div>
+  );
+}
---
+++ src/pages/MoviePage.jsx
@@ -0,0 +1,7 @@
+export default function MoviePage() {
+  return (
+    <div>
+      <h1>Pagina Dettaglio Film</h1>
+    </div>
+  );
+}

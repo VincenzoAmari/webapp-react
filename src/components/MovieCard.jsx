1fcb27b5b7ce07e29cc5c23bc32a2602b71faffd
VincenzoAmari <vincenzo.am99@gmail.com>
2025-03-18 18:01:50 +0100

    modificato movie card per le immagini

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,9 +1,19 @@
 export default function MovieCard({ movie }) {
   return (
     <div className="card">
-      <h3>{movie.title}</h3>
-      <p>Direttore: {movie.director}</p>
-      <p>Anno: {movie.release_year}</p>
+      {movie.image && (
+        <img
+          src={movie.image}
+          alt={movie.title}
+          className="card-img-top"
+          style={{ maxWidth: "200px" }}
+        />
+      )}
+      <div className="card-body">
+        <h3 className="card-title">{movie.title}</h3>
+        <p className="card-text">Direttore: {movie.director}</p>
+        <p className="card-text">Anno: {movie.release_year}</p>
+      </div>
     </div>
   );
 }

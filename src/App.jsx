50b35c0d42422428a12eaf1eb716eaf3f34b88f0
VincenzoAmari <vincenzo.am99@gmail.com>
2025-03-20 16:59:22 +0100

    app update

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,38 @@
+import { createContext, useContext, useState } from "react"; //funzioni per il Context
 import DefaultLayout from "./layouts/DefaultLayout";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import MoviePage from "./pages/MoviePage";
+import CreateMoviePage from "./pages/CreateMoviePage";
+import Loader from "./components/Loader";
+
+// Context
+const LoaderContext = createContext();
+
+// Hook personalizzato per usare il Context
+export function useLoader() {
+  const context = useContext(LoaderContext);
+  if (!context) {
+    throw new Error("useLoader deve essere usato all'interno di App");
+  }
+  return context;
+}
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<DefaultLayout />}>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/movies/:id" element={<MoviePage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <LoaderContext.Provider value={{ isLoading, setIsLoading }}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<DefaultLayout />}>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/movies/:id" element={<MoviePage />} />
+            <Route path="/movies/create" element={<CreateMoviePage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </LoaderContext.Provider>
   );
 }
 
